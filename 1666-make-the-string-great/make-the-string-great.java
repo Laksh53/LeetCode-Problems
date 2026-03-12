32fac6f77d41fcb4e208bb7e0969f9f4bc68adb0
Laksh53 <thulasilakshmanan291005@gmail.com>
2026-03-12 10:24:00 +0530

    Time: 3 ms (64.50%) | Memory: 43.8 MB (36.31%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String makeGood(String s) {
+        if(s.length() == 1 || s.length() == 0)
+        {
+            return s;
+        }
+        Stack<Character> st = new Stack<>();
+        for(char ch : s.toCharArray())
+        {
+           if(!st.isEmpty() && (st.peek() - ch == 32 || st.peek() - ch == -32))
+           {
+                st.pop();
+           }
+           else
+           {
+                st.push(ch);
+           }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(char ch : st)
+        {
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
